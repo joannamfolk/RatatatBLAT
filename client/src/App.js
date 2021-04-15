c37ddb073104386e5f465efb7b15bb3de050e405
Kristin Jue <kristin.jue@gmail.com>
2021-04-14 22:08:47 -0700

    made adjustments to practice what the playing field might look like

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Our team aims to create a digitized version of a board game for the Arc Institute of Learning.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/joannamfolk/RatatatBLAT"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
