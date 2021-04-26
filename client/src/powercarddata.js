8fff0dcf07cf8cc2470ea179c527750412fdc854
dangpatrick <pdang2@mail.greenriver.edu>
2021-04-26 16:09:37 -0700

    added power cards and displayed to browser

---
+++ client/src/powercarddata.js
@@ -0,0 +1,28 @@
+/**
+ * Author: Patrick Dang
+ * Description: Power cards for ratatatcat 
+ */
+
+ const data = [
+    //draw 2 power card
+    {
+      front: 'draw2.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card draw2',
+      caption: 'Card draw2'
+    },
+    //peek power card
+    {
+      front: 'peek.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card peek',
+      caption: 'Card peek'
+    },
+    //swap power card
+    {
+      front: 'swap.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card swap',
+      caption: 'Card swap'
+    },
+  ]; 
