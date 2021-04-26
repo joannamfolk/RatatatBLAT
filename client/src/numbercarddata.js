dd27da693c945d27f0cc23e1aada621acd18146d
Jo <jfolk2@mail.greenriver.edu>
2021-04-25 21:21:26 -0700

    Added number cards to game

---
+++ client/src/numbercarddata.js
@@ -0,0 +1,62 @@
+const data = [
+    {
+      front: '0.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 0',
+      caption: 'Card 0'
+    },
+    {
+      front: '1.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 1',
+      caption: 'Card 1'
+    },
+    {
+      front: '2.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 2',
+      caption: 'Card 2'
+    },
+    {
+      front: '3.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 3',
+      caption: 'Card 3'
+    },
+    {
+      front: '4.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 4',
+      caption: 'Card 4'
+    },
+    {
+      front: '5.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 5',
+      caption: 'Card 5'
+    },
+    {
+      front: '6.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 6',
+      caption: 'Card 6'
+    },
+    {
+      front: '7.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 7',
+      caption: 'Card 7'
+    },
+    {
+      front: '8.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 8',
+      caption: 'Card 8'
+    },
+    {
+      front: '9.jpg',
+      back: 'cardback.jpg',
+      altText: 'Card 9',
+      caption: 'Card 9'
+    }
+  ];
