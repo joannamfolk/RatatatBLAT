1e8ffb5e6dcc874890a76b90ae2c61030f83a3fc
treetrunkz <ecarl@mail.greenriver.edu>
2021-04-28 07:06:53 -0700

    cards state separated and raised to the user's onClick interaction

--- client/src/App.js
+++ client/src/App.js
@@ -3,69 +3,48 @@ import './App.css';
 import Ratatat from './Ratatat.js';
 import RatatatCard from './components/RatatatCard';
 import RatatatBackCard from './components/RatatatBackCard';
-
+import ReactCardFlip from "react-card-flip";
 import ReactFlipCard from 'react-card-flip';
-import data from './numbercarddata.js';
+import Data from './numbercarddata.js';
 import Draggable from 'react-draggable';
 
- 
-// eslint-disable-next-line no-unused-vars
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      isFlipped: false
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick(e){
-    e.preventDefault();
-    this.setState(prevState => ({ isFlipped: !prevState.isFlipped}))
-  }
-  render() {
-    const mystyle = {
-      width: "20%"
-    };
-    let cards = data.map((item)=>{
-      return(
-      <div style={mystyle} id="imgs">
-      <ReactFlipCard isFlipped={this.state.isFlipped} flipDirection="horizontal">
-      <div onClick={this.handleClick}className="card-body">
-      <RatatatCard image={`appImages/${item.front}`}></RatatatCard>
-      </div>
+const CardStyle = {
+  border: "1px solid black",
+  width: "150px",
+  height: "180px"
+};
 
-      <div onClick={this.handleClick} className="card-body">
-      <RatatatBackCard image={`appImages/${item.back}`}></RatatatBackCard>
-      </div>
-      </ReactFlipCard>
-      </div>
-        
-      )
-    })
-    return (
-
-    <div className="App">
-    <div className="App-body">
-      {cards}
-
-
-      <div> +=========+</div>
-      {/* <ReactFlipCard isFlipped={this.state.isFlipped} flipDirection="horizontal">
-      <div onClick={this.handleClick}className="card-body">
-      <RatatatCard image={CardImage} name="card"></RatatatCard>
-      </div>
-     
-    
-
-      <div onClick={this.handleClick} className="card-body">
-      <RatatatBackCard image={CardImage2} name="card"></RatatatBackCard>
-      </div>
-      </ReactFlipCard>
-    */}
+const Card = ({ project }) => {
+  const [isFlipped, setIsFlipped] = React.useState(false);
+  // console.log(project);
+  return (
+    <div style={CardStyle} id="imgs">
+    <ReactFlipCard isFlipped={isFlipped} flipDirection="horizontal">
+    <div onClick={() => setIsFlipped((prev) => !prev)}
+      className="card-body"
+    >
+    <RatatatCard image={`appImages/${project.front}`}></RatatatCard>
     </div>
-  </div>
+
+    <div onClick={() => setIsFlipped((prev) => !prev)} className="card-body">
+    <RatatatBackCard image={`appImages/${project.back}`}></RatatatBackCard>
+    </div>
+    </ReactFlipCard>
+    </div>
   );
 };
-}
-export default App;
+
+const Projects = () => {
+  return (
+    
+    <div style={{ margin: "auto", padding: "20%" }} className="Projects">
+      <h1>RatatatBlat!</h1>
+      {Data.map((item, index) => (
+        <Card image={`appImages/${item.front}`} project={item} key={`card-${index}`} />
+      ))}
+    </div>
+  );
+};
+
+export default Projects;
