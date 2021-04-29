818ea798da8655ba4077e25da6d0946fc2695d00
treetrunkz <ecarl@mail.greenriver.edu>
2021-04-29 09:00:26 -0700

    draggable components doubleclick-flip

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import Ratatat from './Ratatat.js';
 import RatatatCard from './components/RatatatCard';
 import RatatatBackCard from './components/RatatatBackCard';
-import ReactCardFlip from "react-card-flip";
 import ReactFlipCard from 'react-card-flip';
 import Data from './numbercarddata.js';
+
 import Draggable from 'react-draggable';
 
 
 const CardStyle = {
+  
   width: "150px",
   height: "180px"
 };
 
 const Card = ({ project }) => {
+
   const [isFlipped, setIsFlipped] = React.useState(false);
-  // console.log(project);
+  // This is to define what the Card object is (dynamically) 
   return (
+    <Draggable>
     <div style={CardStyle} id="imgs">
     <ReactFlipCard isFlipped={isFlipped} flipDirection="horizontal">
-    <div onClick={() => setIsFlipped((prev) => !prev)}
+    <div onDoubleClick={() => setIsFlipped((prev) => !prev)}
       className="card-body"
     >
     <RatatatCard image={`appImages/${project.front}`}></RatatatCard>
     </div>
 
-    <div onClick={() => setIsFlipped((prev) => !prev)} className="card-body">
+    <div onDoubleClick={() => setIsFlipped((prev) => !prev)} className="card-body">
     <RatatatBackCard image={`appImages/${project.back}`}></RatatatBackCard>
     </div>
     </ReactFlipCard>
     </div>
+    </Draggable>
   );
 };
 
-const Projects = () => {
+const Deck = () => {
   return (
-    <div style={{background: 'darkgreen'}}>
+    
     <div style={{ margin: "auto", padding: "20%" }} className="Projects">
       <h1>RatatatBlat!</h1>
       {Data.map((item, index) => (
         <Card image={`appImages/${item.front}`} project={item} key={`card-${index}`} />
       ))}
     </div>
-    </div>
   );
 };
 
-export default Projects;
+export default Deck;
