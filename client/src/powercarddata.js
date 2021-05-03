bc22b2a68bd0a1d750f7b70483197967ac8ded72
Jo <jfolk2@mail.greenriver.edu>
2021-05-03 12:23:23 -0700

    updated format of numbercard powercard

--- client/src/powercarddata.js
+++ client/src/powercarddata.js
@@ -6,22 +6,22 @@
  const data = [
     //draw 2 power card
     {
-      front: 'draw2.jpg',
-      back: 'cardback.jpg',
+      front: 'draw2.png',
+      back: 'cardback.png',
       altText: 'Card draw2',
       caption: 'Card draw2'
     },
     //peek power card
     {
-      front: 'peek.jpg',
-      back: 'cardback.jpg',
+      front: 'peek.png',
+      back: 'cardback.png',
       altText: 'Card peek',
       caption: 'Card peek'
     },
     //swap power card
     {
-      front: 'swap.jpg',
-      back: 'cardback.jpg',
+      front: 'swap.png',
+      back: 'cardback.png',
       altText: 'Card swap',
       caption: 'Card swap'
     },
